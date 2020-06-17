6b046101cce6a7fa9256f3223928e3fb9b53388b
tjdntjr123 <tjdntjr123@douzone.com>
2020-06-17 18:07:40 +0900

    마테리얼 유아이 푸시

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={browserHistory}>
+    <Route path ="/" component={App}>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
-
 
